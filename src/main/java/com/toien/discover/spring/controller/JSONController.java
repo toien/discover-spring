509389133179ecc33046dc09ee27f544bbaf5881
Toien Liu <lshuhuan@yeah.net>
2015-08-07 01:41:14 +0800

    daily commit

--- src/main/java/com/toien/discover/spring/controller/JSONController.java
+++ src/main/java/com/toien/discover/spring/controller/JSONController.java
@@ -1,9 +1,9 @@
 package com.toien.discover.spring.controller;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +17,6 @@ import com.toien.discover.spring.vo.Person;
 public class JSONController {
 	@RequestMapping(value="/map", method = RequestMethod.GET)
 	public Map<String, Object> map() {
-
 		Map<String, Object> result = new HashMap<String, Object>();
 
 		result.put("id", 1L);
