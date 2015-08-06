3aec72f06ca8cfa671fc8d6a69561068b8b5fbe0
Toien Liu <lshuhuan@yeah.net>
2015-08-06 23:17:50 +0800

    add Jackson to classpath, fix HttpMessageConverter bug

--- src/main/java/com/toien/discover/spring/controller/JSONController.java
+++ src/main/java/com/toien/discover/spring/controller/JSONController.java
@@ -3,17 +3,17 @@ package com.toien.discover.spring.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.toien.discover.spring.vo.Person;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/json")
 public class JSONController {
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Map<String, Object> getUser(
-			@PathVariable(value = "id") String id) {
+	@RequestMapping(value="/map", method = RequestMethod.GET)
+	public Map<String, Object> map() {
 
 		Map<String, Object> result = new HashMap<String, Object>();
 
@@ -23,5 +23,23 @@ public class JSONController {
 
 		return result;
 
+	}
+	@RequestMapping(value="/string", method = RequestMethod.GET)
+	public Map<String, Object> string() {
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		result.put("id", 1L);
+		result.put("name", "Toien");
+		result.put("age", 11);
+		
+		return result;
+		
+	}
+	@RequestMapping(value="/vo", method = RequestMethod.GET)
+	public Person dto() {
+		
+		return new Person("SSH", 133);
+		
 	}
 }
---
+++ src/main/java/com/toien/discover/spring/vo/Person.java
@@ -0,0 +1,23 @@
+package com.toien.discover.spring.vo;
+
+public class Person {
+	String name;
+	int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+}
