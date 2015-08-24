352c003363b679d187048e21107d0afdf4d23079
Toien Liu <lshuhuan@yeah.net>
2015-08-25 00:32:06 +0800

    dc

--- src/main/java/com/toien/discover/spring/config/MvcConfiguration.java
+++ src/main/java/com/toien/discover/spring/config/MvcConfiguration.java
@@ -3,6 +3,8 @@ package com.toien.discover.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,29 +15,37 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import com.toien.discover.spring.interceptor.WrappedResponseInterceptor;
 
 @Configuration
-@ComponentScan(basePackages="com.toien.discover.spring")
+@ComponentScan(basePackages = "com.toien.discover.spring")
 @EnableWebMvc
-public class MvcConfiguration extends WebMvcConfigurerAdapter{
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
-	public ViewResolver getViewResolver(){
+	public ViewResolver getViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
 	
+	@Bean(name = "multipartResolver")
+	public MultipartResolver getMultipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setMaxUploadSize(6 * 1024 * 1024L);
+		resolver.setMaxInMemorySize(3 * 1024 * 1024);
+		return resolver;
+	}
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**", "/**").addResourceLocations("/resources/", "/");
+		registry.addResourceHandler("/resources/**", "/**").addResourceLocations(
+				"/resources/", "/");
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		
+
 		registry.addInterceptor(new WrappedResponseInterceptor());
-		
+
 	}
 
-	
 }
